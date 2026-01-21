b830860344e733233a23ae30734058ecf295f887
2150n91u1 <110704657+2150n91u1@users.noreply.github.com>
2026-01-21 21:55:00 +0800

    Add files via upload

---
+++ Canteen_Utensils_Collector_Machine_Main_Controller.ino
@@ -0,0 +1,134 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#include <LiquidCrystal.h>
+
+#define SS_PIN 53
+#define RST_PIN 49
+
+#define pwm 6
+#define dir 7
+ 
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+MFRC522::MIFARE_Key key; 
+
+LiquidCrystal lcd(22,23,24,25,26,27);
+
+// Init array that will store new NUID 
+byte nuidPICC[4];
+
+int relay=48;
+
+int relay1=46;
+
+void setup() { 
+  lcd.begin(16,2);
+
+  pinMode(relay,OUTPUT);
+  pinMode(relay1,OUTPUT);
+
+  pinMode(pwm,OUTPUT);
+  pinMode(dir,OUTPUT);
+
+  digitalWrite(relay1,HIGH);
+  
+  SPI.begin(); // Init SPI bus
+  rfid.PCD_Init(); // Init MFRC522 
+
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+}
+
+
+void loop() {
+  lcd.clear();
+  lcd.home();
+  lcd.println("Hi there!");
+  delay(1000);
+  lcd.setCursor(0,1);
+  lcd.print("FEED ME PLEASE!");
+  delay(1000);
+  // Look for new cards
+  if ( ! rfid.PICC_IsNewCardPresent())
+    return;
+
+  // Verify if the NUID has been readed
+  if ( ! rfid.PICC_ReadCardSerial())
+    return;
+    
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+
+  // Check is the PICC of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    return;
+  }
+
+  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
+    rfid.uid.uidByte[1] != nuidPICC[1] || 
+    rfid.uid.uidByte[2] != nuidPICC[2] || 
+    rfid.uid.uidByte[3] != nuidPICC[3] ) {
+
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++) {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+
+    staff(rfid.uid.uidByte, rfid.uid.size);
+    //Serial.println();
+  }
+  else{
+    staff(rfid.uid.uidByte, rfid.uid.size);
+    //Serial.println();
+  }
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+}
+
+void staff(byte *buffer, byte bufferSize){
+    if(buffer[0]==21){
+      if(buffer[1]==90){
+        if(buffer[2]==20){
+          if(buffer[3]==9){
+            lcd.clear();
+            lcd.home();
+            lcd.println("Thanks mate!");
+            delay(1000);
+            lcd.setCursor(0,1);
+            lcd.println("Digesting");
+            delay(4000);
+            digitalWrite(pwm,HIGH);
+            delay(250);
+            digitalWrite(pwm,LOW);
+            delay(1000);
+            digitalWrite(relay,HIGH);
+            delay(3600);
+            digitalWrite(relay,LOW);
+            delay(4500);
+            digitalWrite(relay1,LOW);
+            delay(3000);
+            digitalWrite(relay1,HIGH);
+            delay(3000);
+            digitalWrite(relay,HIGH);
+            delay(8500);
+            digitalWrite(relay,LOW);
+            delay(4500);
+          }
+        }
+      }
+    }
+          else {
+            lcd.clear();
+            lcd.home();
+            lcd.println("EWW");
+            delay(1000);
+            lcd.setCursor(0,1);
+            lcd.print("I DON'T EAT THIS");
+            delay(4000);
+        }
+}
---
+++ Canteen_Utensils_Collector_Machine_Sub_Controller.ino
@@ -0,0 +1,124 @@
+#define BLYNK_PRINT Serial
+#define BLYNK_MAX_SENDBYTES 256
+
+#define trigPin1 5
+#define echoPin1 4
+#define trigPin2 7
+#define echoPin2 6
+#define trigPin3 9
+#define echoPin3 8
+#define trigPin4 11
+#define echoPin4 10
+
+
+
+#include <ESP8266_Lib.h>
+#include <BlynkSimpleShieldEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "4fdb96cbfad040f6b5aafdddb94840bc";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "HUAWEI P10 lite";
+char pass[] = "12345678";
+
+long duration, distance, CoinsLevel,PlatesLevel,UtensilsLevel,CupsLevel;
+
+#include <SoftwareSerial.h>
+SoftwareSerial EspSerial(2, 3); // RX, TX
+
+// Your ESP8266 baud rate:
+#define ESP8266_BAUD 9600
+
+ESP8266 wifi(&EspSerial);
+
+BlynkTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V0, PlatesLevel);
+  Blynk.virtualWrite(V1, CupsLevel);
+  Blynk.virtualWrite(V2, UtensilsLevel);
+  Blynk.virtualWrite(V3, CoinsLevel);
+  Blynk.virtualWrite(V4, CoinsLevel);
+}
+
+const int buzzer = 12; 
+const int sensor = 13;
+
+int state;  // 0 close - 1 open wwitch
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  EspSerial.begin(ESP8266_BAUD);
+  delay(10);
+
+  Blynk.begin(auth, wifi, ssid, pass);
+  
+  timer.setInterval(1000L, myTimerEvent);
+
+  Serial.begin (9600);
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+  pinMode(trigPin3, OUTPUT);
+  pinMode(echoPin3, INPUT);
+  pinMode(trigPin4, OUTPUT);
+  pinMode(echoPin4, INPUT);
+
+  pinMode(sensor, INPUT_PULLUP);
+}
+
+void loop()
+{
+  state = digitalRead(sensor);
+  Blynk.run();
+  timer.run(); // Initiates BlynkTimer
+  SonarSensor(trigPin1, echoPin1);
+  CoinsLevel = 15-distance;
+  if(CoinsLevel<3){
+    Blynk.notify("Coins Storage running low, please refill!");
+  }
+  SonarSensor(trigPin2, echoPin2);
+  PlatesLevel = 20-distance;
+
+  SonarSensor(trigPin3, echoPin3);
+  UtensilsLevel = 15-distance;
+  Serial.println(PlatesLevel);
+
+  SonarSensor(trigPin4, echoPin4);
+  CupsLevel = 15-distance;
+  
+  if (state == HIGH){
+    tone(buzzer, 400);
+    delay(1000);
+    noTone(buzzer);
+  }
+  else{
+    noTone(buzzer);
+  }
+  delay(200);
+}
+
+void SonarSensor(int trigPin,int echoPin)
+{
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance = (duration/2) / 29.1;
+ 
+}
